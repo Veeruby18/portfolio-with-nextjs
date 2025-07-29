b345cc0809d5f2dd8cd10950f54baa478e8345a9
Veeruby18 <odueloghen@gmail.com>
2025-07-29 15:56:27 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>About Me</h1>
+      <p>Write your bio here.</p>
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript / TypeScript</li>
+        <li>React / Next.js</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  );
+}
